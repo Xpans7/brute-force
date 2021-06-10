e8f2809ebfd32e71516c7a80956513316c495298
Xpans7_04 <68559038+Xpans7@users.noreply.github.com>
2021-06-11 05:54:30 +0700

    Update Brute-force.py

--- Brute-force.py
+++ Brute-force.py
@@ -1,21 +1,25 @@
 import random
 import string
 
+jawab = 'y'
+while(jawab == "y"):
 
-chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    chars = "0123456789"
 
 # chars = string.printable
-chars_list = list(chars)
+    chars_list = list(chars)
 
 
-password = input("Enter a password : ")
-guess_password = ""
+    password = input("Enter a password : ")
+    guess_password = ""
+    hitung = 0
+    while(guess_password != password):
+        hitung += 1
+        guess_password = random.choices(chars_list, k=len(password))
+        print("Cracking password"+ str(guess_password))
 
-while(guess_password != password):
-    guess_password = random.choices(chars_list, k=len(password))
-
-    print("<=================="+ str(guess_password)+ "==================>")
-
-    if(guess_password == list(password)):
-        print("Your password is : "+ "".join(guess_password))
-        break
+        if(guess_password == list(password)):
+            print("Your password is : "+ "".join(guess_password))
+            break
+    print("percobaan ke"+ "-"+ str(hitung))
+    jawab = input("ingin memulai lagi?[y/t] : ")
